3aa7b66d1d23a4a527adabb3e0a56110ef889543
Robin Tram <robin.tram@gmail.com>
2018-09-24 05:59:59 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -5,13 +5,11 @@ def fourth_challenge
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
-        Benvolio: {
-          name: "Benvolio",
+        { name: "Benvolio",
           age: "17",
           attitude: "worried"
         },
-        Mercutio: {
-          name: "Mercutio",
+        { name: "Mercutio",
           age: "18",
           attitude: "hot-headed"
         }
@@ -22,14 +20,12 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        Steven: {
-          name: "Steven",
+        { name: "Steven",
           age: "30",
           attitude: "confused"
         },
-        Nurse: {
-          name: "Nurse",
-          age: "44",
+        { name: "Nurse",
+          page: "44",
           attitude: "worried"
         }
       ]
